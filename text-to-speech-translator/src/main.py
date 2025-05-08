ea32203bf1cfea285a60e1672a7924ff75539a03
A00227608 <141940305+A00227608@users.noreply.github.com>
2025-05-08 16:27:43 -0600

    Modificar main para utilizar texto de la imagen y eliminar requirements

--- text-to-speech-translator/src/main.py
+++ text-to-speech-translator/src/main.py
@@ -5,29 +5,46 @@ import asyncio
 from translator import Translator
 from text_to_speech import TextToSpeech
 from utils.language_codes import LANGUAGE_CODES
+from ocr import extraer_texto_de_imagen
 
 async def main():
-    print("Welcome to the Text-to-Speech Translator!")
+    print("Welcome to the Text-to-Speech Translator with OCR!")
     
-    # Display available languages
-    print("Available languages:")
+    # Mostrar idiomas disponibles
+    print("Opciones de idioma:")
     for lang in LANGUAGE_CODES.keys():
         print(f"- {lang}")
 
-    source_lang = input("Enter the source language: ").strip().lower()
-    target_lang = input("Enter the target language: ").strip().lower()
+    source_lang = input("Introduce idioma de origen: ").strip().lower()
+    target_lang = input("Introduce idioma para traducir: ").strip().lower()
     
     if source_lang not in LANGUAGE_CODES or target_lang not in LANGUAGE_CODES:
-        print("Invalid language selection. Please try again.")
+        print("Lenguaje no válido")
         sys.exit(1)
 
-    text = input("Enter the text to translate: ")
 
+    image_path = input("Dirección de la imágen: ").strip()
+
+    # Extraer texto
+    print("Extrayendo texto...")
+    try:
+        text = extraer_texto_de_imagen(image_path)
+        if not text.strip():
+            print("No se encontró texto en la imagen.")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error leyendo el texto: {e}")
+        sys.exit(1)
+
+    print(f"Texto extraído: {text}")
+
+    # Traducir el texto
     translator = Translator()
     translated_text = await translator.translate_text(LANGUAGE_CODES[source_lang], LANGUAGE_CODES[target_lang], text)
 
     print(f"Translated text: {translated_text}")
 
+    # Convertir a TTS
     tts = TextToSpeech()
     tts.speak(translated_text, LANGUAGE_CODES[target_lang])
 
--- text-to-speech-translator/src/text_to_speech.py
+++ text-to-speech-translator/src/text_to_speech.py
@@ -8,15 +8,14 @@ class TextToSpeech:
         audio_file = "output.mp3"
         tts.save(audio_file)
 
-        # Initialize pygame mixer
+    
         pygame.mixer.init()
         pygame.mixer.music.load(audio_file)
         pygame.mixer.music.play()
 
-        # Wait until the audio finishes playing
         while pygame.mixer.music.get_busy():
             continue
 
-        # Clean up
+     
         pygame.mixer.quit()
         os.remove(audio_file)
